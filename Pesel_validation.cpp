0197799c59146f3bc8b8d0e8c6c0fb5ec4d5ce73
Patrycja Z <patrycja.zet3@gmail.com>
2022-07-30 22:13:14 +0200

    Initial commit

---
+++ Pesel_validation.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <regex>
+
+bool checkPESEL(const std::string& pesel)
+{
+    std::regex peselRegex("\\d{11}");
+    return std::regex_match(pesel, peselRegex);
+}
+int main()
+{
+    std::string pesel1("123456789");
+    std::string pesel2("345678901");
+    std::string pesel3("ABd5679321567");
+    std::string definitelyNotPesel("34m56b67j34");
+
+    std::cout << pesel1 << " wynik: " << checkPESEL(pesel1) << std::endl;
+    std::cout << pesel1 << " wynik: " << checkPESEL(pesel2) << std::endl;
+    std::cout << pesel1 << " wynik: " << checkPESEL(pesel3) << std::endl;
+    std::cout << definitelyNotPesel << " wynik: " << checkPESEL(definitelyNotPesel) << std::endl;
+}
+
+
